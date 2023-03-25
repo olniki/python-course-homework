219e27220f34fb4917dffa6f80d5e40607787ffd
ovnik <ov.nikishova@gmail.com>
2023-03-24 22:47:26 -0700

    lesson 11: task with cat + optional - fixed flake8

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ def create_cats_dic(number):
         result[i] = False
     return result
 
+
 def go_round(num_circles, number_of_cats):
     cats_dic = create_cats_dic(number_of_cats)
     for i in range(1, num_circles+1):
         for j in range(1, number_of_cats+1, i):
-            if cats_dic[j] == False:
+            if not cats_dic[j]:
                 cats_dic[j] = True
             else:
                 cats_dic[j] = False
     return cats_dic
 
-result = go_round(100,100)
-for cat,value in result.items():
-    if value == True:
-        print(f'Cat # {cat} is in hat')
 
+result = go_round(100, 100)
+for cat, value in result.items():
+    if value:
+        print(f'Cat # {cat} is in hat')
