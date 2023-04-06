72beb0202bffb86da85eceb4f70a6a4a00fb4e05
ovnik <ov.nikishova@gmail.com>
2023-04-06 16:35:39 -0700

    lesson 14:

--- main.py
+++ main.py
@@ -1,93 +1,47 @@
-import inspect
+# 1. Modify the Country class to include a third instance attribute called capital as a string.
+# Store your new class in a script and test it out by adding the following code at the bottom of the script:
+# japan = Country('Japan', 140_000_000, 'Tokyo')
+# print(f"{japan.name} population is {japan.population} and capital is {japan.capital}.")
+# The output of your script should be:
+# Japan population is 140000000 and capital is Tokyo.
+
+# 2. Add increase_population method to country class.
+# This method should take an argument and increase population of the country on this number.
+
+# 3. Create add method to add two countries together.
+# This method should create another country object with the name of the
+# two countries combined and population of the two countries added together.
+
+# 4.(Optional) Implement previous method with magic method
+class Country:
+    def __init__(self, name: str, population: int, capital: str = ''):
+        self.name = name
+        self.population = population
+        self.capital = capital
+
+    def increase_population(self, increase_by):
+        self.population += increase_by
+
+    def add_country(self, second_country):
+        name = self.name + second_country.name
+        population = self.population + second_country.population
+        return Country(name, population)
+
+    def __add__(self, second_country):
+        name = self.name + second_country.name
+        population = self.population + second_country.population
+        return Country(name, population)
 
 
-# Write a decorator that ensures a function is only called by users with a specific role.
-# Each function should have an user_type with a string type in kwargs
-def is_admin(func):
-    def wrapper(*args, **kwargs):
-        if kwargs['user_type'] == 'admin':
-            func(*args, **kwargs)
-        else:
-            print('Permission denied')
+japan = Country('Japan', 140_000_000, 'Tokyo')
+print(f"{japan.name} population is {japan.population} and capital is {japan.capital}.")
+japan.increase_population(111)
+print(f"{japan.name} population is {japan.population} and capital is {japan.capital}.")
+bosnia = Country('Bosnia', 10_000_000)
+herzegovina = Country('Herzegovina', 5_000_000)
 
-    return wrapper
+new_country = bosnia.add_country(herzegovina)
+print(f"{new_country.name} population is {new_country.population}")
 
-
-@is_admin
-def show_customer_receipt(user_type: str):
-    print('Successfully executed')
-
-
-show_customer_receipt(user_type='user')
-
-
-# 2.Write a decorator that wraps a function in a try-except block and print an error if error has happened
-def catch_errors(func):
-    def wrapper(data):
-        try:
-            func(data)
-        except (KeyError) as e:
-            print('Found 1 error during execution of your function: ', e)
-
-    return wrapper
-
-
-@catch_errors
-def some_function_with_risky_operation(data):
-    print(data['key'])
-
-
-some_function_with_risky_operation({'key': 'bar'})
-
-# 3. Optional: Create a decorator that will check types. It should take a function with arguments and validate inputs with annotations.
-
-
-def check_types(func):
-    def wrapper(*args, **kwargs):
-        full_arg_spec = inspect.getfullargspec(func)
-        i = 0
-        args_list = full_arg_spec.args
-        args_type = full_arg_spec.annotations
-        for arg in args_list:
-            if (type(args[i]) != args_type[arg]):
-                raise ValueError('Please check arguments types')
-            i += 1
-            func(*args, **kwargs)
-
-    return wrapper
-
-
-@check_types
-def add(a: int, b: int) -> int:
-    return a + b
-
-
-add(1, 2)
-
-# Create a function that caches the result of a function, so that if it is called with same argument multiple
-# times, it returns the cached result first instead of re-executing the function.
-# It`s one of the real task on the project
-cache = {}
-
-
-def memorize(func):
-    def wrapper(num):
-        if num in cache:
-            print('Returned From Cache: ', cache[num])
-        else:
-            cache[num] = func(num)
-            print('Calculated: ', cache[num])
-    return wrapper
-
-
-@memorize
-def sum(num: int) -> int:
-    sum = 0
-    for i in range(1, num + 1):
-        sum += i
-    return sum
-
-
-sum(100)
-sum(200)
-sum(100)
+magic_country = bosnia+herzegovina
+print(f"{magic_country.name} population is {magic_country.population}")
